a6f3680e9698ca1bc62c32a83472690188071f3d
James Calcaben <jcalcaben+github@gmail.com>
2016-04-10 21:44:35 -0500

    Initial maven files commit

---
+++ src/main/java/jcalcaben/playing_cards/App.java
@@ -0,0 +1,13 @@
+package jcalcaben.playing_cards;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
